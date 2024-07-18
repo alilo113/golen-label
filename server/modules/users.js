b901590694baaa7e6328ade608662cba5abe73f7
unknown <aliopdandan@gmail.com>
2024-07-18 11:33:15 +0100

    f

--- server/modules/users.js
+++ server/modules/users.js
@@ -2,5 +2,20 @@ const mongoose = require(mongoose)
 const bcrypt = require("bcrypt")
 
 const userSchema = new mongoose.schema({
-    
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String, 
+        required: true,
+        unique: true
+    }
 })
+
