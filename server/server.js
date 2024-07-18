03756d1745aa5e474f04ae2827596d728fc04600
unknown <aliopdandan@gmail.com>
2024-07-18 11:27:02 +0100

    f

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 const express = require("express")
 const app = express()
 const port = 3000
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb://127.0.0.1:27017/database")
+.then(() => console.log("Database connected"))
+.catch((error) => console.log(error))
 
 app.get("/api", (req, res) => {
     res.json({message: "This is from the backend"})
